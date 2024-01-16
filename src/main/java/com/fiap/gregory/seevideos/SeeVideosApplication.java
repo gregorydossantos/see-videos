80fdb95b2f262514dab78aebba43be0fcd5ad312
gregory <gregescribano@gmail.com>
2024-01-15 23:01:14 -0300

    create project

---
+++ src/main/java/com/fiap/gregory/seevideos/SeeVideosApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.gregory.seevideos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeVideosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeeVideosApplication.class, args);
+	}
+
+}
